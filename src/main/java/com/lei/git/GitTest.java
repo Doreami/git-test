d9a922adbe9dc59435588ce1faf998cbf4573a45
Doream <1531388357@qq.com>
2022-04-19 17:02:21 +0800

    first commit

---
+++ src/main/java/com/lei/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lei.git;
+
+/**
+ * @Author: Doream
+ * @Date: 2022/4/19 16:57
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
